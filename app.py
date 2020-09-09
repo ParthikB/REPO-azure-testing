e15d6f97c72cc08f74fb596ede9ad7fdac659037
unknown <connect2parthik@gmail.com>
2020-09-09 18:00:17 +0530

    checking auto deployment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World...What's up!!!!!!!!"
+    return "Hello World...What's up!!!!!!!! Everything's fine?"
 
 if __name__ == '__main__':
     app.run(debug=True)
